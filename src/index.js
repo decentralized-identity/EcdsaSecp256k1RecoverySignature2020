789b74d87ae4185b1a8f06651f90b392b81e63de
Orie Steele <orie@transmute.industries>
2020-04-25 10:31:28 -0500

    test bundle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  ["ES256K-R"]: require("./ES256K-R"),
   EcdsaSecp256k1RecoveryMethod2020: require("./EcdsaSecp256k1RecoveryMethod2020"),
   EcdsaSecp256k1RecoverySignature2020: require("./EcdsaSecp256k1RecoverySignature2020"),
 };
