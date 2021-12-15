22d8352d3ba7147c7c878139f1600378de84adf8
Charles E. Lehner <charles.lehner@spruceid.com>
2021-12-15 16:03:18 -0500

    Load w3id and v2 URIs

--- src/__tests__/__fixtures__/customDocumentLoader.js
+++ src/__tests__/__fixtures__/customDocumentLoader.js
@@ -2,20 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const jsonld = require("jsonld");
 
+function readJson(filename) {
+  return JSON.parse(fs.readFileSync(path.resolve(__dirname, filename), 'utf8'));
+}
+
+const recovery2020_0_0 = readJson("../../../lds-ecdsa-secp256k1-recovery2020-0.0.jsonld");
+const recovery2020_2_0 = readJson("../../../lds-ecdsa-secp256k1-recovery2020-2.0.jsonld");
+
 const contexts = {
   "https://www.w3.org/ns/did/v1": require("./contexts/did-core-v1.json"),
   "https://www.w3.org/2018/credentials/v1": require("./contexts/credentials-v1.json"),
   "https://w3c-ccg.github.io/vc-examples/covid-19/v1/v1.jsonld": require("./contexts/covid-19.json"),
-  "https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/lds-ecdsa-secp256k1-recovery2020-0.0.jsonld": JSON.parse(
-    fs
-      .readFileSync(
-        path.resolve(
-          __dirname,
-          "../../../lds-ecdsa-secp256k1-recovery2020-0.0.jsonld"
-        )
-      )
-      .toString()
-  ),
+  "https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/lds-ecdsa-secp256k1-recovery2020-0.0.jsonld": recovery2020_0_0,
+  "https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/lds-ecdsa-secp256k1-recovery2020-2.0.jsonld": recovery2020_2_0,
+  "https://w3id.org/security/suites/secp256k1recovery-2020/v1": recovery2020_0_0,
+  "https://w3id.org/security/suites/secp256k1recovery-2020/v2": recovery2020_2_0,
 };
 
 const customLoader = (url) => {
